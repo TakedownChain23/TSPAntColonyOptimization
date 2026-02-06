2ed7470e9945cafb1b823ac74b7dfda9690c144f
Kyan Grimm <Kyan.J.Grimm@student.uts.edu.au>
2026-02-06 11:19:26 +1100

    Filled out a skeleton of Held Karp algorithm

--- TravellingSalesman/HeldKarp.cs
+++ TravellingSalesman/HeldKarp.cs
@@ -2,35 +2,45 @@ namespace TravellingSalesman;
 
 public class HeldKarp
 {
-    Tuple<int, List<int>> RunAlgorithm(List<List<int>> nodeDistances, int startNode)
+    Tuple<int, List<int>> RunAlgorithm(List<List<int>> nodeDistances)
     {
         var nodeCount = nodeDistances.Count;
-
-        object memoTable = "2D table of size n by 2^n";
+        var subsetCount = 1 << nodeCount;
+        var badResultPlaceholder = int.MaxValue / 4; // Represents an incredibly suboptimal path so any new results will be chosen as best
         
-        Setup(nodeDistances, memoTable, startNode, nodeCount);
-        Solve(nodeDistances, memoTable, startNode, nodeCount);
+        var optimalResults = new List<List<int>>(subsetCount);
+        var tour = new List<List<int>>(subsetCount);
         
-        var minimumCost = FindMinimumCost(nodeDistances, memoTable, startNode, nodeCount);
-        var optimalTour = FindOptimalTour(nodeDistances, memoTable, startNode, nodeCount);
-        return (minimumCost, optimalTour).ToTuple();
-    }
+        for (var i = 0; i < subsetCount; i++)
+        {
+            optimalResults.Add(Enumerable.Repeat(badResultPlaceholder, nodeCount).ToList()); // Initialise results table with bad paths
+            tour.Add(Enumerable.Repeat(-1, nodeCount).ToList()); // Initialise all paths as invalid
+        }
 
-    void Setup(List<List<int>> distances, object memoTable, int startNode, int nodeCount)
-    {
-    }
+        optimalResults[1][0] = 0; // Base case -> takes no distance to get to the start node
 
-    void Solve(List<List<int>> distances, object memoTable, int startNode, int nodeCount)
-    {
-    }
-    
-    int FindMinimumCost(List<List<int>> distances, object memoTable, int startNode, int nodeCount)
-    {
-        return 0;
-    }
+        for (var mask = 1; mask < subsetCount; mask++)
+        {
+            // Does this subset include the starting city
 
-    List<int> FindOptimalTour(List<List<int>> distances, object memoTable, int startNode, int nodeCount)
-    {
+            for (int j = 1; j < nodeCount; j++) // Look through other paths
+            {
+                // Is the city j in the subset?
+                
+                // What is the previous subset if we end at city j
+                var previousMask = mask ^ (1 << j);
+
+                for (int k = 0; k < nodeCount; k++) // Look through previous paths
+                {
+                    // Was city k visited in the previous subset?
+                    
+                    // Is the path via city k cheaper than what we already know?
+                    
+                    // If yes then record new cost and path
+                }
+            }
+        }
+
         return null;
     }
 }
