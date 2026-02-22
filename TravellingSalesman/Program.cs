45b26f87a1bd7e164d299c543706165c23812ad1
TakedownChain23 <isaac.e.thomas@gmail.com>
2026-02-22 14:33:56 +1100

    Updated to only include the Ant Colony Optimization algorithm

--- TravellingSalesman/Program.cs
+++ TravellingSalesman/Program.cs
@@ -16,15 +16,3 @@ stopwatch.Stop();
 Console.WriteLine($"Optimal Path Length: {pathLength}");
 Console.WriteLine($"Optimal Path: {string.Join(" -> ", path)} -> {path[0]}");
 Console.WriteLine($"Execution Time: {stopwatch.Elapsed.TotalSeconds} seconds\n");
-
-stopwatch = Stopwatch.StartNew();
-Console.WriteLine("Running Christofides Algorithm for TSP...\n");
-
-// Run the TSP solver
-var (path2, pathLength2) = Christofides.RunAlgorithm(distanceMatrix);
-stopwatch.Stop();
-
-// Display results
-Console.WriteLine($"Optimal Path Length: {pathLength2}");
-Console.WriteLine($"Optimal Path: {string.Join(" -> ", path2)}");
-Console.WriteLine($"Execution Time: {stopwatch.Elapsed.TotalSeconds} seconds");
